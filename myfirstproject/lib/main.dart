8a0b066d38c06b996aa817423bb106aa3739adf6
Muhammad Vaid <molvy32@gmail.com>
2025-01-16 12:25:16 +0200

    Changed home page title

--- myfirstproject/lib/main.dart
+++ myfirstproject/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Multiple counters Home Page'),
+      home: const MyHomePage(title: 'Multiple counters Home'),
     );
   }
 }
